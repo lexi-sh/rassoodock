2a5f4086fd042274d2aea564ad357a54f5b17eab
Chris Tramel <hello@christophertrml.com>
2017-10-16 23:26:13 -0400

    First try to get this working

--- Rassoodock.SqlServer.Windows.Tests/WhenGettingStoredProcedures.cs
+++ Rassoodock.SqlServer.Windows.Tests/WhenGettingStoredProcedures.cs
@@ -1,9 +1,12 @@
 
 using System;
 using System.Data.SqlClient;
+using System.Linq;
 using Dapper;
 using Rassoodock.Common;
+using Rassoodock.Databases;
 using Rassoodock.Tests.Utilities;
+using Shouldly;
 using Xunit;
 
 namespace Rassoodock.SqlServer.Windows.Tests.Integration
@@ -22,10 +25,28 @@ namespace Rassoodock.SqlServer.Windows.Tests.Integration
                 Name = "TestDatabase"
             };
 
-            var query = $@"IF EXISTS(SELECT * FROM sys.databases WHERE name='{_database.Name}')
-                DROP DATABASE {_database.Name};
-                CREATE DATABASE {_database.Name};";
+            var query = 
+                $"IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name='{_database.Name}') BEGIN CREATE DATABASE {_database.Name}; END";
+            
+            using (var conn = new SqlConnection(_database.ConnectionString))
+            {
+                conn.Open();
+                var exists = conn.ExecuteScalar<bool>("SELECT 1 FROM sys.databases WHERE name = @name",
+                    new
+                    {
+                        name = _database.Name
+                    });
+                if (!exists)
+                {
+                    conn.Execute($"CREATE DATABASE {_database.Name}");
+                }
+            }
+        }
 
+        private void CreateStoredProc(StoredProcedure procedure)
+        {
+            var name = $"[{procedure.Schema}].[{procedure.Name}]";
+            var query = $"CREATE PROC {name} AS BEGIN {procedure.Text} END";
             using (var conn = new SqlConnection(_database.ConnectionString))
             {
                 conn.Open();
@@ -37,8 +58,27 @@ namespace Rassoodock.SqlServer.Windows.Tests.Integration
         public void ShouldBeAbleToGetStoredProceduresForWindowsSqlServer()
         {
             var dbReader = new DatabaseReader(_database);
-            var storedProcs = dbReader.GetStoredProcedures();
 
+            var storedProc1 = new StoredProcedure
+            {
+                Schema = "dbo",
+                Name = "asd123",
+                Text = "SELECT 1"
+            };
+
+            var storedProc2 = new StoredProcedure
+            {
+                Schema = "dbo",
+                Name = "asd1444",
+                Text = "SELECT 'asd'"
+            };
+
+            CreateStoredProc(storedProc1);
+            CreateStoredProc(storedProc2);
+
+            var storedProcs = dbReader.GetStoredProcedures().ToList();
+            storedProcs.FirstOrDefault(x => x.Text == storedProc1.Text).ShouldNotBeNull();
+            storedProcs.FirstOrDefault(x => x.Text == storedProc2.Text).ShouldNotBeNull();
         }
     }
 }
