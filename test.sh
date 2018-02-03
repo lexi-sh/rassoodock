3c62f785b9ef3902f2ddf050383bbcef9276e0f7
Chris Tramel <hello@christophertrml.com>
2018-02-02 19:47:21 -0500

    Modify test shell

--- test.sh
+++ test.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CD $DIR/../
-dotnet build
-
-find $DIR/../test -not -path '*.TestCommon*' -name '*.csproj' -print0 | xargs -L1 -0 -P 8 dotnet test --no-build
-
-if [ "$?" !=  "0" ] ; then
-    echo -e "######### TEST FAILED #########"
-fi
+for dir in PARENT/*
+do
+    cd $dir && dotnet test
+done
