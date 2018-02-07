4cfc5bae0c546dd11ceec0739f5223ba53432f49
Chris Tramel <hello@christophertrml.com>
2018-02-06 23:40:37 -0500

    Add first classes for difference engnie

---
+++ Rassoodock.Cli/ApplyCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Rassoodock.Cli
+{
+    public class ApplyCommand : ICommandExecution
+    {
+        public Action<CommandLineApplication> GenerateCommand()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rassoodock.DifferenceEngnie/IDifferentiator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Rassoodock.DifferenceEngine
+{
+    public interface IDifferentiator<T>
+    {
+        string GetDifferenceAlterString(IEnumerable<T> objectsFromFileSystem, IEnumerable<T> objectsInDb);
+    }
+}
