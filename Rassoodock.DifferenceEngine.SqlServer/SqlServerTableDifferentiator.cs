961dee38581c3c84d2c03e9eca2aa82266258c35
Chris Tramel <hello@christophertrml.com>
2018-04-30 20:55:39 -0400

    Start work on sql server table differentiator

---
+++ Rassoodock.DifferenceEngine.SqlServer/SqlServerTableDifferentiator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Rassoodock.SqlServer.Models.Domain;
+
+namespace Rassoodock.DifferenceEngine.SqlServer
+{
+    public class SqlServerTableDifferentiator : IDifferentiator<SqlServerTable>
+    {
+        public string GetDifferenceAlterString(IEnumerable<SqlServerTable> objectsFromFileSystem, IEnumerable<SqlServerTable> objectsInDb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rassoodock.DifferenceEngine.SqlServer/SqlServerTableStrategies/SqlServerColumnDifferentiationStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Rassoodock.SqlServer.Models.Domain;
+
+namespace Rassoodock.DifferenceEngine.SqlServer.SqlServerTableStrategies
+{
+    public class SqlServerColumnDifferentiationStrategy
+    {
+        private readonly IEnumerable<Column> _Live;
+
+        private readonly IEnumerable<Column> _SourceControl;
+
+        public SqlServerColumnDifferentiationStrategy(IEnumerable<Column> sourceControl, IEnumerable<Column> live)
+        {
+            _SourceControl = sourceControl;
+            _Live = live;
+        }
+
+        public string GetDifferenceAlterString()
+        {
+            return "";
+        }
+    }
+}
